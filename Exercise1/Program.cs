eaaae3455481a9984c73261984136872cc523e9c
Esteban SG <estebandesa0@gmail.com>
2024-09-25 15:54:54 -0600

    add Exercise1

---
+++ Exercise1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+Console.WriteLine("Ingrese el primer numero: ");
+int number1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Ingrese el segundo numero: ");
+int number2 = int.Parse(Console.ReadLine());
+
+int diff = Math.Abs(number1 - number2);
+
+if (number1 > number2)
+{
+    Console.WriteLine(diff * 2);
+}
+else
+{
+    Console.WriteLine(diff);
+}
